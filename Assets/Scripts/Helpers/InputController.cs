a4529ca4fbde57e881333e19161a6b5791a286c3
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2025-01-14 14:50:55 +0300

    Feature
    Simple input controller with swipe detection that works with only in simulator mode

---
+++ Assets/Scripts/Helpers/InputController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+namespace Helpers
+{
+    public class InputController : MonoBehaviour
+    {
+        private Vector2 _startTouchPosition;
+        private Vector2 _endTouchPosition;
+        private bool _isSwipeDetected;
+
+
+        void Update()
+        {
+            DetectSwipe();
+        }
+
+        private void DetectSwipe()
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        _startTouchPosition = touch.position;
+                        _isSwipeDetected = false;
+                        break;
+
+                    case TouchPhase.Moved:
+                    case TouchPhase.Ended:
+                        _endTouchPosition = touch.position;
+
+                        if (!_isSwipeDetected)
+                        {
+                            Vector2 swipeDelta = _endTouchPosition - _startTouchPosition;
+
+                            if (Mathf.Abs(swipeDelta.x) > Mathf.Abs(swipeDelta.y)) // Horizontal swipe
+                            {
+                                if (swipeDelta.x < 0) // Swiping left
+                                {
+                                    Debug.Log("swipe left");
+                                }
+                                else if (swipeDelta.x > 0) // Swiping right
+                                {
+                                    Debug.Log("swipe right");
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
