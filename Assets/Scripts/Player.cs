f8ed63007f4210f12a9db3913362709f2c9f6f48
Ahmet Hakan Hafif <59022934+colegion@users.noreply.github.com>
2025-01-14 16:32:35 +0300

    Refactor
    Removal of unnecessary codeblock

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,4 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Transform stickTarget;
-
-    private void Update()
-    {
-        transform.position = stickTarget.position;
-        transform.rotation = stickTarget.rotation;
-    }
 }
